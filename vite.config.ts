d7c1b6be1933ef49b8ad3988a913cc6aa086dd5a
dualwind <dualwind@qq.com>
2024-01-06 00:10:51 +0800

    vite.config.ts build.rollupOptions.external 增加Vue

--- vite.config.ts
+++ vite.config.ts
@@ -125,7 +125,7 @@ export default defineConfig({
 
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["siyuan", "process"],
+      external: ["siyuan", "process", "Vue"],
 
       output: {
         entryFileNames: "[name].js",
